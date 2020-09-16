f1ff54b487ce0e0b3dd03abe6d2f6ede28039860
Karma Sherpa <ksherpa@mof.gov.bt>
2020-09-16 11:30:52 +0600

    added one more test

--- maven/src/test/java/bt/gov/springboot/maven/maven/MavenApplicationTests.java
+++ maven/src/test/java/bt/gov/springboot/maven/maven/MavenApplicationTests.java
@@ -10,6 +10,13 @@ class MavenApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a=1;
+		int b=1;
+		assertTrue(a+b==2);
+	}
+
+	@Test
+	void contextLoads1() {
 		int a=2;
 		int b=2;
 		assertTrue(a+b==4);
